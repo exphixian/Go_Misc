fafd3f6e19830f6d7d86eb1f629201eaac805a61
Kathleen Wilson <37256276+exphixian@users.noreply.github.com>
2022-03-29 13:44:41 -0600

    Add files via upload

---
+++ API.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Pokemon struct {
+	EntryNo int            `json:"entry_number"`
+	Species PokemonSpecies `json:"pokemon_species"`
+}
+
+type Response struct {
+	Name    string    `json:"name"`
+	Pokemon []Pokemon `json:"pokemon_entries"`
+}
+
+type PokemonSpecies struct {
+	Name string `json:"name"`
+}
+
+func main() {
+	response, err := http.Get("http://pokeapi.co/api/v2/pokedex/kanto/")
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//fmt.Println(string(responseData))
+
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+
+	fmt.Println(responseObject.Name)
+	fmt.Println(len(responseObject.Pokemon))
+
+	for _, pokemon := range responseObject.Pokemon {
+		fmt.Println(pokemon.Species.Name)
+	}
+}
